e26503b86d4852c37f058f1bfb4671e47fdf9ed4
giokuan <giokuan@gmail.com>
2022-09-04 00:20:54 +0800

    fixed the layout of form in add-student.blade

--- resources/views/pages/add-student.blade.php
+++ resources/views/pages/add-student.blade.php
@@ -27,7 +27,7 @@
                 </div>
               </div>
         @endif
-        <form class="w-full max-w-lg pl-8 pr-8 pt-6 pb-4" method="post" action="{{url('save-student')}}">
+        <form class="w-full  md:px-40 pt-6 pb-4" method="post" action="{{url('save-student')}}">
             @csrf
             <div class="flex flex-wrap -mx-3 mb-2">
 
@@ -35,7 +35,7 @@
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-1" for="lastname">
                 Last Name
                 </label>
-                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-2 px-4 mb-3 leading-tight focus:outline-none focus:bg-white shadow-lg" id="lastname" name="lastname" type="text" >
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-2 px-4 mb-3 leading-tight focus:outline-none focus:bg-white shadow-lg" id="lastname" name="lastname" type="text" value="{{old('lastname')}}" >
                 {{-- <p class="text-red-500 text-xs italic">Please fill out this field.</p> --}}
                 @error('lastname')
 
@@ -54,7 +54,19 @@
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-1" for="firstname">
                 first Name
                 </label>
-                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-2 px-4 leading-tight focus:outline-none focus:bg-white shadow-lg focus:border-gray-500" id="firstname" name="firstname" type="text">
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-2 px-4 leading-tight focus:outline-none focus:bg-white shadow-lg focus:border-gray-500" id="firstname" name="firstname" type="text" value="{{old('firstname')}}" >
+                @error('firstname')
+
+                <div class="flex p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800" role="alert">
+                    <svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
+                    <span class="sr-only">Info</span>
+                    <div>
+                      <span class="font-medium"> First Name field is required</span> 
+                    </div>
+                  </div>
+
+                @enderror
+           
             </div>
 
             <div class="w-full md:w-1/3 px-3">
@@ -87,7 +99,21 @@
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-1" for="photo">
                     Photo
                 </label>
-                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-2 px-4 leading-tight focus:outline-none focus:bg-white shadow-lg focus:border-gray-500" id="photo" name="photo" type="text" >
+                <input class="form-control
+                block
+                w-full
+                px-0.75
+                py-0.75
+                text-lg
+                {{-- font-normal --}}
+                text-gray-500
+                bg-gray-200 bg-clip-padding
+                border  
+                rounded
+                transition
+                ease-in-out
+                m-0
+                focus:text-gray-700 focus:bg-white  focus:outline-none shadow-lg" id="formFileLg" type="file">
                 </div>
         
             </div>
