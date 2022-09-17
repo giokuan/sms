26d3e9acc43f9c949010b54e15a006455a990c7f
Michael <michaeldrpabaya@gmail.com>
2022-09-17 09:04:02 +0800

    redirect user after register to login page

--- app/Http/Controllers/Auth/EmailVerificationPromptController.php
+++ app/Http/Controllers/Auth/EmailVerificationPromptController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
+use Auth;
 
 class EmailVerificationPromptController extends Controller
 {
@@ -16,8 +17,25 @@ class EmailVerificationPromptController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return $request->user()->hasVerifiedEmail()
-                    ? redirect()->intended(RouteServiceProvider::HOME)
-                    : view('auth.verify-email');
+
+         if ($request->user()->hasVerifiedEmail())
+        {
+            Auth::guard('web')->logout();
+
+            $request->session()->invalidate();
+    
+            $request->session()->regenerateToken();
+    
+            return redirect()->intended('login');
+        }
+        else
+        {
+            return view('auth.verify-email');
+        }
+                    
+
+        // return $request->user()->hasVerifiedEmail()
+        //             ? redirect()->intended(RouteServiceProvider::HOME)
+        //             : view('auth.verify-email');
     }
 }
