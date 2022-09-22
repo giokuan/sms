d5fb93fc7c50f8cf688bf134b0e991632ab1869d
Michael <michaeldrpabaya@gmail.com>
2022-09-22 14:39:17 +0800

    Polished the Announcement

--- resources/views/teacherHome.blade.php
+++ resources/views/teacherHome.blade.php
@@ -3,24 +3,23 @@
 @extends('partials.__sidebar')
 @section('content')	
 
-  @foreach($querys as $query)
-  <div class="p-10 flex flex-row">
-    <!--Card 1-->
-    <div class=" w-full lg:max-w-full items-center">
-        <div class="mb-8">
-       
-            <p class="text-gray-900 dark:text-gray-100 font-semibold text-xl">{{$query->title}}</p>
+@foreach($querys as $query)
+<div class="p-10 flex flex-row bg-gray-200 dark:bg-gray-800 mb-8 rounded-md ">
+  <!--Card 1-->
+  <div class=" w-full lg:max-w-full items-center">
+     
+          <p class="text-gray-900 dark:text-gray-100 font-semibold text-xl">{{$query->title}}</p>
 
-            <p class="text-gray-800 dark:text-gray-300 font-normal text-base my-2 ">{{$query->message}}</p>
+          <p class="text-gray-800 dark:text-gray-300 font-normal text-base my-2 ">{{$query->message}}</p>
 
-            <div class="flex justify-center">
-              <img src="{{url('uploads/announcement/'.$query->photo)}}" alt="announcement photo">
-            </div>
+          <div class="flex justify-center">
+            <img src="{{url('uploads/announcement/'.$query->photo)}}" alt="announcement photo">
+          </div>
 
-        </div>
-    </div>
   </div>
-  @endforeach
+</div>
+<hr class="mb-5 text-gray-900">
+@endforeach
 
 @endsection
 
