8067ed547ab7d1cc2f72f25a9f58b504abb40018
wayne <107015056+naofumish02@users.noreply.github.com>
2022-09-22 10:11:54 +0800

    contact s

--- resources/views/contactform.blade.php
+++ resources/views/contactform.blade.php
@@ -87,9 +87,9 @@
                             </div>
                      
                             <div class="form-group text-center">
-                                <button type="button" class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">Submit message</button>
-                                {{-- <button type="submit" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"">Send message</button> 
-                                <button class="btn btn-success btn-submit">Submit</button> --}}
+                                {{-- <button type="button" class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">Submit message</button> --}}
+                                 {{-- <button type="submit" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"">Send message</button>  --}}
+                                <button class="btn btn-success btn-submit">Submit</button> 
                             </div>
                         </form>
                     </div>
